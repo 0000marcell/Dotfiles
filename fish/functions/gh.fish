516d0d11970d00b86f4866353bdedf6c59e2d749
Marcell Monteiro Cruz <0000marcell@gmail.com>
2016-02-18 03:10:38 -0200

    github functions were added

---
+++ fish/functions/gh.fish
@@ -0,0 +1,37 @@
+function gh-create-repo
+	if count $argv > /dev/null
+		curl -u "$GITHUB_USER:$GITHUB_TOKEN" https://api.github.com/user/repos -d '{"name":"'$argv[1]'"}'
+	else
+		set_color red
+		echo "my user name is $GITHUB_USER"	
+	end
+end
+
+function gh-push-new-repo
+	if count $argv > /dev/null
+		git init 
+		git add README.md
+		git commit -m "first commit"
+		git remote add origin git@github.com:0000marcell/$argv[1].git
+		git push -u origin master
+	else
+		set_color red
+		echo 'pass the name of the remote repo'
+	end
+end
+
+function gh-commit-push
+	if count $argv > /dev/null
+		git add --all
+		git commit -m "$argv[1]"
+		git push
+	else
+		set_color red
+		echo 'put a msg to commit'
+	end
+end
+
+function gh-commit
+	git add --all
+	git commit -m "$argv[1]"
+end
