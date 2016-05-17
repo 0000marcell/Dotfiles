854bd85b3f261df5fc013e3b12fd3757f64a1575
Marcell Monteiro Cruz <0000marcell@gmail.com>
2016-05-16 22:17:08 -0300

    curl post with header fixed

--- fish/functions/curl-functions.fish
+++ fish/functions/curl-functions.fish
@@ -10,7 +10,7 @@ function curl-json-get
 	curl -v -H  --header $argv[1] $argv[2] 
 end
 
-# header - json - url
+# ex curl-json-post "Authorization: Bearer 1929238" '{"todo": "this shit", "isCompleted": 0}' 'http://0.0.0.0:3000/api/v1/todos'
 function curl-json-post
-	curl -v -H  --header $argv[1] -X POST -d $argv[2] $argv[3] 
+	curl -v -H "Content-Type: application/json" --header $argv[1] -X POST -d $argv[2] $argv[3] 
 end
