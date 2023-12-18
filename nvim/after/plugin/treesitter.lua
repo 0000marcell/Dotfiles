f2847c8cede01e2825d8f832a648f5250dc076b0
____marcell <0000marcell@gmail.com>
2023-12-18 22:50:53 +0000

    more snippets added

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -1,13 +1,13 @@
-require 'nvim-treesitter.configs'.setup {
+--require 'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "javascript", "typescript", "ruby", "lua", "c", "vim", "vimdoc", "markdown" },
+  -- ensure_installed = { "javascript", "typescript", "ruby", "lua", "c", "vim", "vimdoc", "markdown" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = false,
+  --sync_install = false,
 
   -- Automatically install missing parsers when entering buffer
   -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-  auto_install = true,
+  --auto_install = true,
 
   -- List of parsers to ignore installing (for "all")
   -- ignore_install = {  },
@@ -15,8 +15,8 @@ require 'nvim-treesitter.configs'.setup {
   ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
   -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
 
-  highlight = {
-    enable = true,
+  -- highlight = {
+  --   enable = true,
 
     -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
     -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
@@ -28,6 +28,6 @@ require 'nvim-treesitter.configs'.setup {
     -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
     -- Using this option may slow down your editor, and you may see some duplicate highlights.
     -- Instead of true it can also be a list of languages
-    additional_vim_regex_highlighting = false,
-  },
-}
+    --additional_vim_regex_highlighting = false,
+  --},
+--}
--- nvim/init.lua
+++ nvim/init.lua
@@ -84,7 +84,7 @@ return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
 
-  use 'David-Kunz/gen.nvim'
+  --use 'David-Kunz/gen.nvim'
 
   use('preservim/nerdtree')
 
@@ -133,13 +133,13 @@ return require('packer').startup(function(use)
 
   }
 
-  use {
-    'nvim-treesitter/nvim-treesitter',
-    run = function()
-      local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
-      ts_update()
-    end,
-  }
+  -- use {
+  --   'nvim-treesitter/nvim-treesitter',
+  --   run = function()
+  --     local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+  --     ts_update()
+  --   end,
+  -- }
 
 
   -- Debugging
