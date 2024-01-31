f1019068ce9d0a3fa4bb844c8e65a463dbb38f51
____marcell <0000marcell@gmail.com>
2024-01-31 23:00:05 +0000

    more git maps

--- nvim/after/plugin/vim-fugitive.lua
+++ nvim/after/plugin/vim-fugitive.lua
@@ -1,1 +1,4 @@
 vim.keymap.set("n", "<leader>ga", ":G add %<CR>")
+vim.keymap.set("n", "<leader>gg", ":G<CR>")
+vim.keymap.set("n", "<leader>gc", ":G commit<CR>")
+vim.keymap.set("n", "<leader>gp", ":G push<CR>")
