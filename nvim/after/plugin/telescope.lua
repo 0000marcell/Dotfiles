090802c6cc8a678b4ca816d13aa719df51a16e4e
____marcell <0000marcell@gmail.com>
2024-03-13 20:18:08 +0000

    add note to resume

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -2,6 +2,7 @@ vim.g.mapleader = " "
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fs', builtin.git_files, {})
+-- this is the one that I always forget
 vim.keymap.set('n', '<leader>fr', builtin.resume, {})
 vim.keymap.set("n", "<leader>fg", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>")
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
