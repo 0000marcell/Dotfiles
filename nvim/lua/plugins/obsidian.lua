8beba2258ad5416756b9454ecf1993fcd30960ef
Marcell Cruz <0000marcell@gmail.com>
2026-04-26 15:25:42 -0300

    update dotfiles

--- nvim/lua/plugins/obsidian.lua
+++ nvim/lua/plugins/obsidian.lua
@@ -6,6 +6,19 @@ return {
   dependencies = {
     "nvim-lua/plenary.nvim",
   },
+  keys = {
+    { "<leader>oo", "<cmd>ObsidianOpen<cr>", desc = "Open in Obsidian" },
+    { "<leader>on", "<cmd>ObsidianNew<cr>", desc = "New note" },
+    { "<leader>os", "<cmd>ObsidianSearch<cr>", desc = "Search notes" },
+    { "<leader>oq", "<cmd>ObsidianQuickSwitch<cr>", desc = "Quick switch" },
+    { "<leader>of", "<cmd>ObsidianFollowLink<cr>", desc = "Follow link" },
+    { "<leader>ob", "<cmd>ObsidianBacklinks<cr>", desc = "Backlinks" },
+    { "<leader>ot", "<cmd>ObsidianTags<cr>", desc = "Tags" },
+    { "<leader>od", "<cmd>ObsidianToday<cr>", desc = "Today's daily note" },
+    { "<leader>oy", "<cmd>ObsidianYesterday<cr>", desc = "Yesterday's daily note" },
+    { "<leader>ol", "<cmd>ObsidianLinks<cr>", desc = "Links in current note" },
+    { "<leader>op", "<cmd>ObsidianPasteImg<cr>", desc = "Paste image" },
+  },
   opts = {
     workspaces = {
       {
