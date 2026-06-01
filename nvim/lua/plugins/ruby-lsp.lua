3b507d3db0508f42d67d0673406f0eff92ad9450
Marcell Cruz <0000marcell@gmail.com>
2026-06-01 15:00:37 -0300

    update dotfiles

--- nvim/lua/plugins/ruby-lsp.lua
+++ nvim/lua/plugins/ruby-lsp.lua
@@ -1,38 +1,59 @@
 return {
   {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        ruby_lsp = {
-          cmd = { "bundle", "exec", "ruby-lsp" },
-          filetypes = { "ruby" },
-          root_dir = require("lspconfig.util").root_pattern("Gemfile", ".git"),
-          init_options = {
-            formatter = "auto",
-            enabledFeatures = {
-              "documentHighlights",
-              "documentSymbols",
-              "foldingRanges",
-              "selectionRanges",
-              "semanticHighlighting",
-              "formatting",
-              "codeActions",
-              "diagnostics",
-              "completion",
-              "definition",
-              "workspaceSymbol",
-            },
-            experimentalFeaturesEnabled = true,
-          },
-          settings = {},
-        },
-      },
+    "adam12/ruby-lsp.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "neovim/nvim-lspconfig",
     },
-  },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      vim.list_extend(opts.ensure_installed, { "ruby" })
+    config = function()
+      require("ruby-lsp").setup({
+        -- Forwards configurations to lspconfig's ruby_lsp setup
+        lspconfig = {
+          init_options = {
+            formatter = "rubocop", -- or 'standard'
+            linters = { "rubocop" },
+          },
+        },
+      })
     end,
   },
 }
+
+-- return {
+--   {
+--     "neovim/nvim-lspconfig",
+--     opts = {
+--       servers = {
+--         ruby_lsp = {
+--           cmd = { "ruby-lsp" },
+--           filetypes = { "ruby" },
+--           root_dir = require("lspconfig.util").root_pattern("Gemfile", ".git"),
+--           init_options = {
+--             formatter = "auto",
+--             enabledFeatures = {
+--               "documentHighlights",
+--               "documentSymbols",
+--               "foldingRanges",
+--               "selectionRanges",
+--               "semanticHighlighting",
+--               "formatting",
+--               "codeActions",
+--               "diagnostics",
+--               "completion",
+--               "definition",
+--               "workspaceSymbol",
+--             },
+--             experimentalFeaturesEnabled = true,
+--           },
+--           settings = {},
+--         },
+--       },
+--     },
+--   },
+--   {
+--     "nvim-treesitter/nvim-treesitter",
+--     opts = function(_, opts)
+--       vim.list_extend(opts.ensure_installed, { "ruby" })
+--     end,
+--   },
+-- }
